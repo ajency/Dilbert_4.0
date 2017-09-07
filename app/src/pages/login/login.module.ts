4b418b45320c530480c54289cfa344dc40ffb3d6
Ajaj Rajguru <ajaj@ajency.in>
2017-09-07 15:43:04 +0530

    loader added on login

--- app/src/pages/login/login.module.ts
+++ app/src/pages/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 import { AppServiceProvider } from '../../providers/app-service/app-service';
+import { LoggedInHeaderComponentModule } from '../../components/logged-in-header/logged-in-header.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppServiceProvider } from '../../providers/app-service/app-service';
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
+    LoggedInHeaderComponentModule
   ],
   exports: [
     LoginPage
