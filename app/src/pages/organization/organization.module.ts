f3670e40cf0dbeaea8b4e26cf859b8d132620e1a
Ajaj Rajguru <ajaj@ajency.in>
2017-09-07 18:41:30 +0530

    organization page styled

--- app/src/pages/organization/organization.module.ts
+++ app/src/pages/organization/organization.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { OrganizationPage } from './organization';
+import { LoggedInHeaderComponentModule } from '../../components/logged-in-header/logged-in-header.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { OrganizationPage } from './organization';
   ],
   imports: [
     IonicPageModule.forChild(OrganizationPage),
+    LoggedInHeaderComponentModule
   ],
   exports: [
     OrganizationPage
