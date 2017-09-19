c6e41f4e05d7f42814057693391940ca83f9309a
Sharang Sinai Amonkar <sharang@ajency.in>
2017-09-19 11:51:37 +0530

    fixed new-organisation and shifted functions to model

--- api/app/Http/Controllers/Ajency/User/SocialAuthController.php
+++ api/app/Http/Controllers/Ajency/User/SocialAuthController.php
@@ -124,7 +124,7 @@ class SocialAuthController extends Controller {
 
                         $output->writeln(count($user_resp['user_details']));
 
-                        return response()->json($this->getNextUrl($account,$user_resp,$response));
+                        return response()->json((new Organisation)->getNextUrl($account,$user_resp,$response,$_SERVER['REMOTE_ADDR']));
 
                     }
                     else {
@@ -166,89 +166,5 @@ class SocialAuthController extends Controller {
             return true;
         else
             return false;
-    }
-
-    /**
-     * Once the user is created determines the the next url and the appropriate
-     * data to be sent in the reponse.
-     * @param  SocialiteObject $account   [description]
-     * @param  $user_resp contains user_details, user_comm and user objects
-     * @param  $response pre-initialised response array
-     * @return response to be sent
-     */
-    public function getNextUrl($account,$user_resp,$response) {
-        $output = new ConsoleOutput;
-        $output->writeln("");
-        $output->writeln("getNextUrl");
-        $output->writeln("");
-
-        // $user_resp['user'] = $user_resp['user']->first();
-        $email = $user_resp['user_comm']->first()->value;
-
-        $output->writeln("Email");
-        $output->writeln($email);
-        $output->writeln("");
-
-        // $user_resp['user_details'] = $user_resp['user_details']->first();
-
-        $output->writeln("getnexturl - user_details");
-        $output->writeln(json_encode($user_resp["user_details"]));
-        $output->writeln("");
-
-        $output->writeln("getnexturl - required_fields_filled");
-        $output->writeln(json_encode($user_resp["required_fields_filled"]));
-        $output->writeln("");
-
-        $user_resp['user_details'] = $user_resp['user_details']->first();
-        // check if the organisation exists
-        $org = Organisation::where('domain',$account->user['domain']);
-        if($org->exists()) {
-            // if($user_resp['user']->required_fields_filled['field_required']) {
-            if($user_resp['required_fields_filled']['filled_required']) {
-                // go to dashboard
-                $response['next_url'] = "/dashboard";
-                $response['status'] = 200;
-                $response['message'] = "Go to dashboard.";
-                $response['data'] = [
-                    'user_id' => $user_resp['user']->first()->id,
-                    'userEmail' => $email,
-                    'x_api_key' => $user_resp['user_details']->api_token,
-                ];
-                return $response;
-            }
-            else {
-                $orgDetails = $org->first();
-                // join an organisation
-                $response['next_url'] = "/join_organisation";
-                $response['status'] = 200;
-                $response['message'] = "Join an organisation.";
-                $response['data'] = [
-                    'email' => $email,
-                    'user_id' => $user_resp['user']->id,
-                    'x_api_key' => $user_resp['user_details']->api_token,
-                    'company' => $orgDetails->name,
-                    'domain' => $orgDetails->domain,
-                    // 'timeZones' => array_merge(array($orgDetails->default_tz), unserialize($orgDetails->alt_tz)),    //merge default & alt
-                    'timeZones' => $orgDetails->default_tz,
-                ];
-                return $response;
-            }
-        }
-        else {
-            // create a new organisation
-            $response['next_url'] = "new_organisation";
-            $response['status'] = 200;
-            $response['message'] = "Create a new organisation.";
-            $output->writeln("count user_resp: ".count($user_resp));
-            $output->writeln($user_resp['user']->id);
-            $response['data'] = [
-                'email' => $email,
-                'user_id' => $user_resp['user']->first()->id,
-                'x_api_key' => $user_resp['user_details']->api_token,
-                'domain' => $account->user['domain'],
-                'ip' => $_SERVER['REMOTE_ADDR'],
-            ];
-            return $response;
-        }
     }
 }
