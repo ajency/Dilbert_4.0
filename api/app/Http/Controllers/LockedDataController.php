b9fa9a8e79ec94b54c0404e106075a5964c611e0
Sharang Sinai Amonkar <sharang@ajency.in>
2017-09-21 17:37:41 +0530

    added permission checks to day-summary and period-data

--- api/app/Http/Controllers/LockedDataController.php
+++ api/app/Http/Controllers/LockedDataController.php
@@ -12,6 +12,7 @@ use App\Log;
 use App\Organisation;
 use App\Role;
 use App\Permission;
+use Ajency\User\Ajency\userauth\UserAuth;
 
 use Symfony\Component\Console\Output\ConsoleOutput;
 
@@ -29,76 +30,82 @@ class LockedDataController extends Controller
         App::setLocale($locale);
         if(!empty($request->user_id) && !empty($request->input('filters.date_range')) && $request->header('X-API-KEY')!= null) {
             if(UserDetail::where(['api_token' => $request->header('X-API-KEY'), 'user_id' =>$request->header('from')])->count() != 0) {
-                // when some valid user accesses this api
-                // check if the user_id exists in the users table
-                if(User::where('id',$request->user_id)->count() != 0)
-                    $user = UserDetail::where('user_id',$request->user_id)->first();
-                else
-                    return response()->json(['status' => 400, 'message' => __('api_messages.user_dne')]);
-                // if($user->can('edit-personal')) {        // role check removed for now
-                    // user has permissions to view this data
-                    // get the organisation details
-                    $orgDetails = Organisation::where('id',$user->org_id)->first();
-                    // check if the start and end dates are given
-                    if($request->has('filters.date_range.start') && $request->has('filters.date_range.end')) {
-                        // if both start and end are given
-                        $startDate = new \DateTime($request->input('filters.date_range.start'));
-                        $endDate = new \DateTime($request->input('filters.date_range.end'));
-                    }
-                    else if($request->has('filters.date_range.start')) {
-                        // if only start is given
-                        if(!empty($request->input('filters.period_unit')))
-                            $periodUnit = $request->input('filters.period_unit');
-                        else
-                            $periodUnit = $orgDetails->period_unit;
-                        //startDate and endDate will be the first and the last day of the org_period
-                        $date = explode('-',$request->input('filters.date_range.start'));
-                        if($periodUnit == 'week') {
-                            $sdWeekNo = new \DateTime($request->input('filters.date_range.start'));
-                            $sdWeekNo = $sdWeekNo->format('W');
-                            $startDate = new \DateTime();
-                            $startDate = $startDate->setISODate($date[0],$sdWeekNo)->setTime(0,0);
-                            $endDate = clone $startDate;
-                            $endDate->modify('+6 days');
-                        }
-                        else if ($periodUnit == 'month'){
-                            $startDate = new \DateTime($date[0].'-'.$date[1].'-01');
-                            $endDate = new \DateTime($date[0]."-".$date[1]."-".cal_days_in_month(CAL_GREGORIAN,$date[1],$date[0]));
-                        }
-                        else
-                            //just in case the organisation's period_unit is not set
-                            return response()->json(['status' => 400, 'message' => __('api_messages.org_period_unit')]);
-                    }
-
-                    //get all the user details from the locked_data table
-                    // return response()->json(['count' => count($lockedData)]);
-                    $data = [];
-                    $data['user_id'] = $request->user_id;
-
-                    //get latest data (current data)
-                    $currentData = Locked_Data::where(['user_id' => $request->user_id, 'work_date' => date('Y-m-d')])->get();
-                    $output->writeln(count($currentData));
-                    $cDayData = (new Locked_Data)->formattedLockedData($request->user_id,$currentData);
-                    $data['current'] = $cDayData;
-
-                    // if sorting options are set
-                    if(isset($request->sort_by))
-                        $sortCol = $request->sort_by;
+                // when some valid user accesses this api check if the calling user has the right permissions
+                $callingUser = User::where('id',$request->header('from'))->first();
+                if($callingUser->can('edit-personal')) {
+                    // check if the user_id exists in the users table
+                    if(User::where('id',$request->user_id)->count() != 0)
+                        $user = UserDetail::where('user_id',$request->user_id)->first();
                     else
-                        $sortCol = 'work_date';
-                    if(isset($request->sort_order))
-                        $sortOrder = $request->sort_order;
-                    else
-                        $sortOrder = 'desc';
-                    // user's data for the particular period
-                    $lockedData = Locked_Data::where('user_id',$request->user_id)->whereBetween('work_date',[$startDate->format('Y-m-d'), $endDate->format('Y-m-d')])->orderBy($sortCol, $sortOrder)->get();
-                    $periodData = (new Locked_Data)->formattedLockedData($request->user_id,$lockedData);
-                    $data['periodData'] = $periodData;
-                    return response()->json(['status' => 200, 'message' => __('api_messages.user_periodic_data'), 'data' => $data]);
-                // }
-                // else {
-                //     return response()->json(['status' => 400, 'message' => __('api_messages.permissions')]);
-                // }
+                        return response()->json(['status' => 400, 'message' => __('api_messages.user_dne')]);
+                    // if($user->can('edit-personal')) {        // role check removed for now
+                        // user has permissions to view this data
+                        // get the organisation details
+                        $orgDetails = Organisation::where('id',$user->org_id)->first();
+                        // check if the start and end dates are given
+                        if($request->has('filters.date_range.start') && $request->has('filters.date_range.end')) {
+                            // if both start and end are given
+                            $startDate = new \DateTime($request->input('filters.date_range.start'));
+                            $endDate = new \DateTime($request->input('filters.date_range.end'));
+                        }
+                        else if($request->has('filters.date_range.start')) {
+                            // if only start is given
+                            if(!empty($request->input('filters.period_unit')))
+                                $periodUnit = $request->input('filters.period_unit');
+                            else
+                                $periodUnit = $orgDetails->period_unit;
+                            //startDate and endDate will be the first and the last day of the org_period
+                            $date = explode('-',$request->input('filters.date_range.start'));
+                            if($periodUnit == 'week') {
+                                $sdWeekNo = new \DateTime($request->input('filters.date_range.start'));
+                                $sdWeekNo = $sdWeekNo->format('W');
+                                $startDate = new \DateTime();
+                                $startDate = $startDate->setISODate($date[0],$sdWeekNo)->setTime(0,0);
+                                $endDate = clone $startDate;
+                                $endDate->modify('+6 days');
+                            }
+                            else if ($periodUnit == 'month'){
+                                $startDate = new \DateTime($date[0].'-'.$date[1].'-01');
+                                $endDate = new \DateTime($date[0]."-".$date[1]."-".cal_days_in_month(CAL_GREGORIAN,$date[1],$date[0]));
+                            }
+                            else
+                                //just in case the organisation's period_unit is not set
+                                return response()->json(['status' => 400, 'message' => __('api_messages.org_period_unit')]);
+                        }
+
+                        //get all the user details from the locked_data table
+                        // return response()->json(['count' => count($lockedData)]);
+                        $data = [];
+                        $data['user_id'] = $request->user_id;
+
+                        //get latest data (current data)
+                        $currentData = Locked_Data::where(['user_id' => $request->user_id, 'work_date' => date('Y-m-d')])->get();
+                        $output->writeln(count($currentData));
+                        $cDayData = (new Locked_Data)->formattedLockedData($request->user_id,$currentData);
+                        $data['current'] = $cDayData;
+
+                        // if sorting options are set
+                        if(isset($request->sort_by))
+                            $sortCol = $request->sort_by;
+                        else
+                            $sortCol = 'work_date';
+                        if(isset($request->sort_order))
+                            $sortOrder = $request->sort_order;
+                        else
+                            $sortOrder = 'desc';
+                        // user's data for the particular period
+                        $lockedData = Locked_Data::where('user_id',$request->user_id)->whereBetween('work_date',[$startDate->format('Y-m-d'), $endDate->format('Y-m-d')])->orderBy($sortCol, $sortOrder)->get();
+                        $periodData = (new Locked_Data)->formattedLockedData($request->user_id,$lockedData);
+                        $data['periodData'] = $periodData;
+                        return response()->json(['status' => 200, 'message' => __('api_messages.user_periodic_data'), 'data' => $data]);
+                    // }
+                    // else {
+                    //     return response()->json(['status' => 400, 'message' => __('api_messages.permissions')]);
+                    // }
+                }
+                else {
+                    return response()->json(["message" => "you dont have the right permissions."]);
+                }
             }
             else {
                 return response()->json(['status' => 400, 'message' => __('api_messages.authorisation')]);
--- api/app/Http/Controllers/LogsController.php
+++ api/app/Http/Controllers/LogsController.php
@@ -27,88 +27,95 @@ class LogsController extends Controller
         App::setLocale($locale);
         if(!empty($request->user_id) && !empty($request->date)) {
             if(UserDetail::where(['api_token' => $request->header('X-API-KEY'), 'user_id' =>$request->header('from')])->count() != 0) {
-                // check if a change of state offset is given
-                if(isset($request->cos_offset))
-                    $cosOffset = $request->cos_offset;
-                else
-                    $cosOffset = 0;
-                // acquire the data to be sent
-                $data = [];
-                // pass basic user data for next api request
-                $data['user'] = ['user_id' => $request->user_id, 'x_api_key' => $request->header('X-API-KEY')];
-                // get the days data from locked__datas -------------- call that new function
-                $daysData = Locked_Data::where(['user_id' => $request->user_id, 'work_date' => $request->date])->get();
-                $data['day_data'] = (new Locked_Data)->formattedLockedData($request->user_id,$daysData);  // formatted locked data
-                // get the user's day logs
-                $logs = [];
-                $userLogs = Log::where(['user_id' => $request->user_id, 'work_date' => $request->date])->get(); // all logs
-                $state = null;
-                $start = null;
-                $end = null;
-                foreach($userLogs as $log) {
-                    // if this is the start of the startDate
-                    if($start == null && $log->to_state == 'New Session') {   // New Session = active
-                            $state = 'active';
-                            $start = $log->cos;
-                            continue;
-                    }
-                    // signifying the end of the offline state
-                    else if($state == 'offline' && $log->to_state == 'New Session') {
-                        $end = $log->cos;
-                        if((new Log)->timeDifferenceInMins($start,$end) >= $cosOffset) {
-                            array_push($logs,['state' => $state, 'start_time' => $start, 'end_time' => $end, 'state_time' => (new Log)->timeDifferenceInMins($start,$end)]);
-                            $state = 'active';
-                            $start = $log->cos;
-                            $end = null;
-                            continue;
-                        }
-                        else {
-                            // edit the end time of the previous record
-                            $lastRecord = array_pop($logs);
-                            if($lastRecord == null) {
-                                // if this the first entry to the logs array
-                                $end = null;
-                            }
-                            else {
-                                // ignore the current record and use the previous one
-                                $state = $lastRecord['state'];
-                                $start = $lastRecord['start_time'];
-                                $end = null;
-                            }
-                        }
-                    }
-                    // detects the end of the state and also curbs multiple
-                    // offline entries from being reported
-                    if(($log->from_state == $state || $log->to_state == 'offline') && $state != 'offline') {
-                        $end = $log->cos;
-                        // check if this change of state is to be passed
-                        if((new Log)->timeDifferenceInMins($start,$end) >= $cosOffset) {
-                            array_push($logs,['state' => $state, 'start_time' => $start, 'end_time' => $end, 'state_time' => (new Log)->timeDifferenceInMins($start,$end)]);
-                            $state = $log->to_state;
-                            $start = $log->cos;
-                            $end = null;
-                        }
-                        else {
-                            // edit the end time of the previous record
-                            $lastRecord = array_pop($logs);
-                            if($lastRecord == null) {
-                                // if this the first entry to the logs array
-                                $end = null;
-                            }
-                            else {
-                                // ignore the current record and use the previous one
-                                $state = $lastRecord['state'];
-                                $start = $lastRecord['start_time'];
-                                $end = null;
-                            }
-                        }
-                    }
+                // check for the user-permissions
+                $callingUser = User::where('id',$request->header('from'))->first();
+                if($callingUser->can('edit-personal')) {
+                    // check if a change of state offset is given
+                    if(isset($request->cos_offset))
+                        $cosOffset = $request->cos_offset;
                     else
-                        continue;
+                        $cosOffset = 0;
+                    // acquire the data to be sent
+                    $data = [];
+                    // pass basic user data for next api request
+                    $data['user'] = ['user_id' => $request->user_id, 'x_api_key' => $request->header('X-API-KEY')];
+                    // get the days data from locked__datas -------------- call that new function
+                    $daysData = Locked_Data::where(['user_id' => $request->user_id, 'work_date' => $request->date])->get();
+                    $data['day_data'] = (new Locked_Data)->formattedLockedData($request->user_id,$daysData);  // formatted locked data
+                    // get the user's day logs
+                    $logs = [];
+                    $userLogs = Log::where(['user_id' => $request->user_id, 'work_date' => $request->date])->get(); // all logs
+                    $state = null;
+                    $start = null;
+                    $end = null;
+                    foreach($userLogs as $log) {
+                        // if this is the start of the startDate
+                        if($start == null && $log->to_state == 'New Session') {   // New Session = active
+                                $state = 'active';
+                                $start = $log->cos;
+                                continue;
+                        }
+                        // signifying the end of the offline state
+                        else if($state == 'offline' && $log->to_state == 'New Session') {
+                            $end = $log->cos;
+                            if((new Log)->timeDifferenceInMins($start,$end) >= $cosOffset) {
+                                array_push($logs,['state' => $state, 'start_time' => $start, 'end_time' => $end, 'state_time' => (new Log)->timeDifferenceInMins($start,$end)]);
+                                $state = 'active';
+                                $start = $log->cos;
+                                $end = null;
+                                continue;
+                            }
+                            else {
+                                // edit the end time of the previous record
+                                $lastRecord = array_pop($logs);
+                                if($lastRecord == null) {
+                                    // if this the first entry to the logs array
+                                    $end = null;
+                                }
+                                else {
+                                    // ignore the current record and use the previous one
+                                    $state = $lastRecord['state'];
+                                    $start = $lastRecord['start_time'];
+                                    $end = null;
+                                }
+                            }
+                        }
+                        // detects the end of the state and also curbs multiple
+                        // offline entries from being reported
+                        if(($log->from_state == $state || $log->to_state == 'offline') && $state != 'offline') {
+                            $end = $log->cos;
+                            // check if this change of state is to be passed
+                            if((new Log)->timeDifferenceInMins($start,$end) >= $cosOffset) {
+                                array_push($logs,['state' => $state, 'start_time' => $start, 'end_time' => $end, 'state_time' => (new Log)->timeDifferenceInMins($start,$end)]);
+                                $state = $log->to_state;
+                                $start = $log->cos;
+                                $end = null;
+                            }
+                            else {
+                                // edit the end time of the previous record
+                                $lastRecord = array_pop($logs);
+                                if($lastRecord == null) {
+                                    // if this the first entry to the logs array
+                                    $end = null;
+                                }
+                                else {
+                                    // ignore the current record and use the previous one
+                                    $state = $lastRecord['state'];
+                                    $start = $lastRecord['start_time'];
+                                    $end = null;
+                                }
+                            }
+                        }
+                        else
+                            continue;
+                    }
+                    array_push($logs,['state' => $state, 'start_time' => $start, 'end_time' => $end, 'state_time' => null]);
+                    $data['logs'] = $logs;
+                    return response()->json(['status' => 200, 'message' => __('api_messages.day_summary'), 'data' => $data]);
                 }
-                array_push($logs,['state' => $state, 'start_time' => $start, 'end_time' => $end, 'state_time' => null]);
-                $data['logs'] = $logs;
-                return response()->json(['status' => 200, 'message' => __('api_messages.day_summary'), 'data' => $data]);
+                else {
+                    return response()->json(["message" => "you dont have the right permissions."]);
+                }
             }
             else
                 return response()->json(['status' => 400, 'message' => __('api_messages.authorisation')]);
