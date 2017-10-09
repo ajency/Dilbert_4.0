c28704c6f19a74131a0bebd1bfd871e3202369dc
Sharang Sinai Amonkar <sharang@ajency.in>
2017-10-09 13:16:00 +0530

    getting status from logs when endtime != null

--- api/app/Locked_Data.php
+++ api/app/Locked_Data.php
@@ -88,13 +88,13 @@ class Locked_Data extends Model
             if($ld->end_time != null) {
                 $endTime = new \DateTime($ld->end_time);
                 $dayData['end_time'] = $endTime->format('H:i');
-                $dayData['status'] = '';
+                $dayData['status'] = $this->getCurrentStatus($user_id,date('Y-m-d'));;
             }
             else {
                 //the day isnt over yet
                 $endTime = new \DateTime();
                 //set as the current time
-                $dayData['end_time'] = $endTime->modify('+5 hour +30 minutes')->format('H:i');
+                $dayData['end_time'] = $endTime/*->modify('+5 hour +30 minutes')*/->format('H:i');
                 $dayData['status'] = $this->getCurrentStatus($user_id,date('Y-m-d'));
             }
             $dayData['total_time'] = date_diff($startTime,$endTime)->format('%h:%i');
@@ -118,7 +118,7 @@ class Locked_Data extends Model
         if($usersLogs->exists()) {
             $usersLogs = $usersLogs->first();
             if($usersLogs->to_state == 'New Session')
-                return 'Offline';
+                return 'active';
             else
                 return $usersLogs->to_state;
         }
