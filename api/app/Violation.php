7a71f8d3d0c1a440811c907102ba8af51e7ba0e5
Sharang Sinai Amonkar <sharang@ajency.in>
2017-10-12 16:09:49 +0530

    integrated the violation package

--- api/app/Violation.php
+++ api/app/Violation.php
@@ -1,42 +1,27 @@
 <?php
 
-namespace App;
+namespace Ajency\Violations\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Organisation_Meta;
 
 class Violation extends Model
 {
+	protected $table = "aj_vio_violations";
+
+	
+	//only allow the following items to be mass-assigned to our model
+	protected $fillable = array('status','who_id','who_type','who_meta','whom_id','whom_type','whom_meta','cc_list','bcc_list' );
+
+
     /**
-     * creates a formatted violation data object for the package
-     * @param  [type] $userData  all the user details
-     * @param  [type] $keyFields violation key fieds
-     * @param  $rhsFields  rhs field keys
-     * @return formatted violation data object
+     * The attributes that should be cast to native types.
+     *
+     * @var array
      */
-    public function createFormattedViolationData($userData,$keyFields,$rhsFields) {
-        $data = [];
-        // all user data
-        $user = $userData['user'];
-        $userDetails = $userData['user_details']->first();
-        $userComm = $userData['user_comm'];
-
-        // generate the violation_data part
-        $violation_data['who_id'] = $user['id'];
-        $violation_data['who_type'] = $user->getRoleNames()->first(); // role
-        $violation_data['who_meta']['email'] = $userComm->where('type','email')->first()->value;
-        $violation_data['who_meta']['name'] = $user['name'];
-        $data['violation_data'] = $violation_data;
-
-        // rule_key_fields
-        $rule_key_fields = $keyFields;
-        $data['rule_key_fields'] = $rule_key_fields;
-
-        // rule_rhs
-        foreach($rhsFields as $rhsf) {
-            $rule_rhs[$rhsf] = (new Organisation_Meta)->getParamValue($rhsf,$userDetails['org_id'],$user['violation_grp_id']);
-        }
-        $data['rule_rhs'] = $rule_rhs;
-        return $data;
-    }
+    protected $casts = [
+        'who_meta' => 'array',
+        'whom_meta' => 'array',
+        'cc_list' => 'array',
+        'bcc_list' => 'array',
+    ];	
 }
