ea144993756f6cbf6c4ba0083da5720daf61cbab
Sharang Sinai Amonkar <sharang@ajency.in>
2017-10-12 16:06:10 +0530

    added grp id to org meta and corrected the getParam function

--- api/app/OrganisationMeta.php
+++ api/app/OrganisationMeta.php
@@ -7,6 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class OrganisationMeta extends Model
 {
     /**
+     * fetches you the parameter value from the Organisation meta table
+     * @param  $key   paramter name
+     * @param  $orgId organisation id
+     * @param  $grpId violation group id
+     * @return returns value of $key
+     */
+    public function getParamValue($key,$orgId,$grpId) {
+        $metaData =  OrganisationMeta::where(['organisation_id' => $orgId, 'group_id' => $grpId, 'key' => $key])->first();
+        if(count($metaData))	//return if single value
+            return $metaData->value;
+        else
+            return null;
+    }
+
+    /**
      * returns all role's meta
      * @param  [type] $orgId oragnisation id
      * @param  [type] $role  role
