99371a2024c0647c8c636aa2205f608beebdc2eb
Sharang Sinai Amonkar <sharang@ajency.in>
2017-10-26 12:41:09 +0530

    state update cron runs from 9:30 am to 9:30 pm ist

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // cron that runs every 5 minutes to check for offline state between 9:30 am and 9:30 pm ist
-        $schedule->call('\App\Http\Controllers\CronController@stateUpdate')->everyFiveMinutes()/*->between("04:00","16:00")*/;
+        $schedule->call('\App\Http\Controllers\CronController@stateUpdate')->everyFiveMinutes()->between("04:00","16:00");
 
         // daily cron for status update
         $schedule->call('\App\Http\Controllers\CronController@daily')->dailyAt("17:00");
