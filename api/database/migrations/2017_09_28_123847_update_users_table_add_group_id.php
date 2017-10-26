63692035f9e12b83c06c0441e9e8506c284fba2f
Sharang Sinai Amonkar <sharang@ajency.in>
2017-10-26 16:32:27 +0530

    default grp id = 0

--- api/database/migrations/2017_09_28_123847_update_users_table_add_group_id.php
+++ api/database/migrations/2017_09_28_123847_update_users_table_add_group_id.php
@@ -14,7 +14,7 @@ class UpdateUsersTableAddGroupId extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('violation_grp_id')->default(1);
+            $table->integer('violation_grp_id')->default(0);
         });
     }
 
