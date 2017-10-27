49eae785f994f719186b566c21e3cb0e7c5b1454
Sharang Sinai Amonkar <sharang@ajency.in>
2017-10-27 13:18:51 +0530

    added monthly and weekly cron entry in kernel.php

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -27,8 +27,16 @@ class Kernel extends ConsoleKernel
         // cron that runs every 5 minutes to check for offline state between 9:30 am and 9:30 pm ist
         $schedule->call('\App\Http\Controllers\CronController@stateUpdate')->everyFiveMinutes()->between("04:00","16:00");
 
-        // daily cron for status update
+        // daily cron for status update - everyday at 10:30 pm
         $schedule->call('\App\Http\Controllers\CronController@daily')->dailyAt("17:00");
+
+        // weekly cron for total week hours violation - every sunday at 10:30 pm
+        $schedule->call('\App\Http\Controllers\CronController@weekly')->sundays()->at('17:00');
+
+        // monthly cron for monthly total hours violation
+        $schedule->call('\App\Http\Controllers\CronController@monthly')->when(function () {
+            return \Carbon\Carbon::now()->endOfMonth()->isToday();
+        });
     }
 
     /**
