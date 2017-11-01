a49fd59c9e6f05a21519af58fcfbdf4c2e358736
Sharang Sinai Amonkar <sharang@ajency.in>
2017-11-01 16:57:29 +0530

    comments

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -27,13 +27,13 @@ class Kernel extends ConsoleKernel
         // cron that runs every 5 minutes to check for offline state between 9:30 am and 9:30 pm ist
         $schedule->call('\App\Http\Controllers\CronController@stateUpdate')->everyFiveMinutes()->between("04:00","16:00");
 
-        // daily cron for status update - everyday at 10:30 pm
+        // daily cron for status update - everyday at 10:30 pm ist
         $schedule->call('\App\Http\Controllers\CronController@daily')->dailyAt("17:00");
 
-        // weekly cron for total week hours violation - every sunday at 10:30 pm
+        // weekly cron for total week hours violation - every sunday at 10:30 pm ist
         $schedule->call('\App\Http\Controllers\CronController@weekly')->sundays()->at('17:00');
 
-        // monthly cron for monthly total hours violation
+        // monthly cron for monthly total hours violation - last day of every month at 10:30 pm ist
         $schedule->call('\App\Http\Controllers\CronController@monthly')->when(function () {
             return \Carbon\Carbon::now()->endOfMonth()->isToday();
         })->at('17:00');
