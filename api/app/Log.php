a4c9836d40274ed475da2cdb0b931a4b989fd03e
Sharang Sinai Amonkar <sharang@ajency.in>
2017-12-07 12:43:43 +0530

    modified day-summary algorithm and shifted to Log model file

--- api/app/Log.php
+++ api/app/Log.php
@@ -23,4 +23,156 @@ class Log extends Model
         $output->writeln($d->i);
         return $d->h*60 + $d->i;
     }
+
+    public function getDaySummary($ip_list, $userLogs, $cosOffset) {
+        $logs = [];
+        $state = null;
+        $start = null;
+        $end = null;
+        foreach($userLogs as $log) {
+            // check if the logs ip belongs to organisation's ip_lists
+            // if not skip the log
+            if(!in_array($log->ip_addr,$ip_list))
+                continue;
+            // if this is the start of the startDate
+            if($start == null && $log->to_state == 'New Session') {   // New Session = active
+                    $state = 'active';
+                    $start = $log->cos;
+                    continue;
+            }
+            // signifying the end of the offline state
+            else if($state == 'offline' && $log->to_state == 'New Session') {
+                $end = $log->cos;
+                if($this->timeDifferenceInMins($start,$end) >= $cosOffset) {
+                    array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => $this->timeDifferenceInMins($start,$end)]);
+                    $state = 'active';
+                    $start = $log->cos;
+                    $end = null;
+                    continue;
+                }
+                else {
+                    // edit the end time of the previous record
+                    $lastRecord = array_pop($logs);
+                    if($lastRecord == null) {
+                        // if this the first entry to the logs array
+                        array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => $this->timeDifferenceInMins($start,$end)]);
+                        $state = $log->to_state;
+                        $start = $log->cos;
+                        $end = null;
+                    }
+                    else {
+                        // ignore the current record and use the previous one
+                        $state = $lastRecord['state'];
+                        $start = $lastRecord['start_time'];
+                        $end = null;
+                    }
+                }
+            }
+            // detects the end of the state and also curbs multiple
+            // offline entries from being reported
+            if(($log->from_state == $state || $log->to_state == 'offline') && $state != 'offline') {
+                $end = $log->cos;
+                // check if this change of state is to be passed
+                if($this->timeDifferenceInMins($start,$end) >= $cosOffset) {
+                    array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => $this->timeDifferenceInMins($start,$end)]);
+                    $state = $log->to_state;
+                    $start = $log->cos;
+                    $end = null;
+                }
+                else {
+                    // edit the end time of the previous record
+                    $lastRecord = array_pop($logs);
+                    if($lastRecord == null) {
+                        // if this the first entry to the logs array
+                        array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => $this->timeDifferenceInMins($start,$end)]);
+                        $state = $log->to_state;
+                        $start = $log->cos;
+                        $end = null;
+                    }
+                    else {
+                        // ignore the current record and use the previous one
+                        $state = $lastRecord['state'];
+                        $start = $lastRecord['start_time'];
+                        $end = null;
+                    }
+                }
+            }
+            else
+                continue;
+        }
+        if($state != null)
+            array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => null]);$logs = [];
+            $state = null;
+            $start = null;
+            $end = null;
+            foreach($userLogs as $log) {
+                // check if the logs ip belongs to organisation's ip_lists
+                // if not skip the log
+                if(!in_array($log->ip_addr,$ip_list))
+                    continue;
+                // if this is the start of the startDate
+                if($start == null && $log->to_state == 'New Session') {   // New Session = active
+                        $state = 'active';
+                        $start = $log->cos;
+                        continue;
+                }
+                // signifying the end of the offline state
+                else if($state == 'offline' && $log->to_state == 'New Session') {
+                    $end = $log->cos;
+                    if($this->timeDifferenceInMins($start,$end) >= $cosOffset) {
+                        array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => $this->timeDifferenceInMins($start,$end)]);
+                        $state = 'active';
+                        $start = $log->cos;
+                        $end = null;
+                        continue;
+                    }
+                    else {
+                        // edit the end time of the previous record
+                        $lastRecord = array_pop($logs);
+                        if($lastRecord == null) {
+                            // if this the first entry to the logs array
+                            $end = null;
+                        }
+                        else {
+                            // ignore the current record and use the previous one
+                            $state = $lastRecord['state'];
+                            $start = $lastRecord['start_time'];
+                            $end = null;
+                        }
+                    }
+                }
+                // detects the end of the state and also curbs multiple
+                // offline entries from being reported
+                if(($log->from_state == $state || $log->to_state == 'offline') && $state != 'offline') {
+                    $end = $log->cos;
+                    // check if this change of state is to be passed
+                    if($this->timeDifferenceInMins($start,$end) >= $cosOffset) {
+                        array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => $this->timeDifferenceInMins($start,$end)]);
+                        $state = $log->to_state;
+                        $start = $log->cos;
+                        $end = null;
+                    }
+                    else {
+                        // edit the end time of the previous record
+                        $lastRecord = array_pop($logs);
+                        if($lastRecord == null) {
+                            // if this the first entry to the logs array
+                            $end = null;
+                        }
+                        else {
+                            // ignore the current record and use the previous one
+                            $state = $lastRecord['state'];
+                            $start = $lastRecord['start_time'];
+                            $end = null;
+                        }
+                    }
+                }
+                else
+                    continue;
+            }
+            if($state != null)
+                array_push($logs,['state' => $state, 'start_time' => substr($start,0,5), 'end_time' => substr($end,0,5), 'state_time' => null]);
+
+            return $logs;
+    }
 }
