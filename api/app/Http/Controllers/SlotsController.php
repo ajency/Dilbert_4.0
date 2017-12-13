ce936e8e1df5412c2c3feb19cd250bbbf0aaa918
Sharang Sinai Amonkar <sharang@ajency.in>
2017-12-13 18:31:45 +0530

    no slots marking

--- api/app/Http/Controllers/SlotsController.php
+++ api/app/Http/Controllers/SlotsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\Console\Output\ConsoleOutput;
+use DateTime;
 
 use App\Slots;
 use App\Log;
@@ -48,27 +49,57 @@ class SlotsController extends Controller
 
             $totalTime = (new Log)->timeDifferenceInMins($slotStart, $slotEnd);
 
-            // create a slots object if it does not exist
-            if($slotEntry->exists())
-                $slotEntry = $slotEntry->first();
-            else
-                $slotEntry = new Slots;
+            if($slotData['type'] != 'no_slot') {
+                // create a slots object if it does not exist
+                if($slotEntry->exists())
+                    $slotEntry = $slotEntry->first();
+                else
+                    $slotEntry = new Slots;
 
-            // make an entry to the slots table
-            $slotEntry->user_id = $request->user_id;
-            $slotEntry->type = $slotData['type'];
-            $slotEntry->work_date = $request->work_date;
-            $slotEntry->start_time = $slotStart;
-            $slotEntry->end_time = $slotEnd;
-            $slotEntry->total_time = ((int)($totalTime/60)).':'.$totalTime%60;
-            $slotEntry->save();
+                // make an entry to the slots table
+                $slotEntry->user_id = $request->user_id;
+                $slotEntry->type = $slotData['type'];
+                $slotEntry->work_date = $request->work_date;
+                $slotEntry->start_time = $slotStart;
+                $slotEntry->end_time = $slotEnd;
+                $slotEntry->total_time = ((int)($totalTime/60)).':'.$totalTime%60;
+                $slotEntry->save();
+            }
+            else {
+                // if 'no_slot' is sent some slot timings may change
+                // get all slots
+                $allSlots = Slots::where(['user_id' => $request->user_id, 'work_date' => $request->work_date])->get();
+
+                $slotStart = new DateTime($slotStart);
+                $slotEnd = new DateTime($slotEnd);
+                foreach($allSlots as $markedSlot) {
+                    $markedSlotStart = new DateTime($markedSlot->start_time);
+                    $markedSlotEnd = new DateTime($markedSlot->end_time);
+                    if($slotStart <= $markedSlotStart && $markedSlotStart <= $slotEnd && $slotEnd <= $markedSlotEnd) {
+                        // start of this slot needs to be changed
+                        $markedSlot->start_time = $slotEnd->format('H:i');
+                        $markedSlot->save();
+                    }
+                    else if($markedSlotStart <= $slotStart && $slotStart <= $markedSlotEnd && $markedSlotEnd <= $slotEnd) {
+                        // end of this slot needs to be modified
+                        $markedSlot->end_time = $slotStart->format('H:i');
+                        $markedSlot->save();
+                    }
+                    else if($slotStart <= $markedSlotStart && $markedSlotStart <= $markedSlotEnd && $markedSlotEnd <= $slotEnd) {
+                        // marked slot lies completely inside no_slot | delete this slot
+                        $markedSlot->delete();
+                    }
+                    else if($markedSlotStart <= $slotStart && $slotStart <= $slotEnd && $slotEnd <= $markedSlotEnd) {
+                        // no_slot lies inside marked slot | throw eror
+                        $message = $message."'No slot' cannot be marked.";
+                    }
+                }
+            }
         }
 
         // check if any error messages exist
-        if($message == '') {
-            // $allSlots = Slots::select('type','work_date','start_time','end_time','total_time')->where(['user_id' => $request->user_id, 'work_date' => $request->work_date])->get();
-        return response()->json(['status' => 200, 'message' => 'Slots added successfully.'/*, 'data' => $allSlots*/]);
-        }
+        if($message == '')
+            return response()->json(['status' => 200, 'message' => 'Slots added successfully.']);
         else
             return response()->json(['status' => 400, 'message' => $message]);
     }
