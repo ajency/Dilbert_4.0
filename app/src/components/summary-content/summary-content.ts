7e3ea02d3d8f03c011fa5e31164d100bc7ea09d4
Anish Gadekar <anish@ajency.in>
2018-03-13 11:45:48 +0530

    log button text changed

--- app/src/components/summary-content/summary-content.ts
+++ app/src/components/summary-content/summary-content.ts
@@ -75,7 +75,7 @@ export class SummaryContentComponent {
       this.titleCasePipe = new TitleCasePipe();
 
       this.updateContentCB = (data) => {
-        $("#changelogview").html("View in details");
+        $("#changelogview").html("View More");
         // this.currentData = data.date;
         this.undoSelection();
         console.log('inside update content');
@@ -649,7 +649,7 @@ changelogview(){
         if ( $(".logbox").hasClass("hidden") ) {
              $(".logbox").removeClass("hidden");
              $(".logbox2").addClass("hidden");
-             $("#changelogview").html("View in details");
+             $("#changelogview").html("View More");
        }
        else{
         $(".logbox").addClass("hidden");
