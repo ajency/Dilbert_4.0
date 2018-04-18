ffa833a442eb935200ab609b400b3730706eeaeb
jalmiShreya <shreya@ajency.in>
2018-04-18 11:37:00 +0530

    cloud function for view leave

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+// require("firebase/firestore");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,18 +8,17 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-var firebase = require('firebase-admin');
+firebase = require('firebase-admin');
 
 var config = {
-	apiKey: "AIzaSyDs1qpxF4o7RCVR724Z7TBnDUCmBW01CeQ",
-	authDomain: "dilbert-34d6c.firebaseapp.com",
-	databaseURL: "https://dilbert-34d6c.firebaseio.com",
-	projectId: "dilbert-34d6c",
-	storageBucket: "dilbert-34d6c.appspot.com",
-	messagingSenderId: "58006331254"
-};
-
-firebase.initializeApp(config);
+    apiKey: "AIzaSyDs1qpxF4o7RCVR724Z7TBnDUCmBW01CeQ",
+    authDomain: "dilbert-34d6c.firebaseapp.com",
+    databaseURL: "https://dilbert-34d6c.firebaseio.com",
+    projectId: "dilbert-34d6c",
+    storageBucket: "dilbert-34d6c.appspot.com",
+    messagingSenderId: "58006331254"
+  };
+  firebase.initializeApp(config);
 
 exports.createLeave = functions.https.onRequest((request,response) => {
 	response.setHeader("Access-Control-Allow-Origin", "*");
@@ -223,3 +223,406 @@ function getRequestData(request) {
 	}
 	return request;
 }
+
+
+exports.displayLeave = functions.https.onRequest((request,response) => {
+	response.setHeader("Access-Control-Allow-Origin", "*");
+	if (request.method === "POST") 
+	{
+	var requestBody = request.body;
+	var responseData=[];
+	var data = [];
+	var leaves;
+
+	console.log('request : ',requestBody);
+	var ref = firebase.database().ref('leave/'+requestBody.filters.users);
+
+	ref.once('value', function (snap) {
+	 	snap.forEach(function (parentSnap) {
+	 		responseData.push(parentSnap.val());
+	 	});
+	 	console.log('Response Data : ',responseData);
+	 	leaves = {
+	 		"leaves" : responseData
+	 	};
+
+	 	var dataResponse = {
+	 			"status" : "success",
+				"message" : "200 OK",
+				"data" : leaves
+		 	}
+
+		console.log('Return Data : ',dataResponse);
+
+	 	response.status(200).send(dataResponse); 
+
+	},function (error) {
+		console.log("Error: " + error.code);
+	});
+	}
+	else
+	{
+		return_value=[
+		{
+			"status" : "error",
+			"message" : "wrong method",
+		}]
+		response.status(200).send(return_value); 
+	}
+});
+
+
+exports.displayLeaveTest1 = functions.https.onRequest((request,response) => {
+	response.setHeader("Access-Control-Allow-Origin", "*");
+	if (request.method === "POST") 
+	{
+	var requestBody = request.body;
+	var responseData=[];
+	var data = [];
+	var leaves;
+
+	console.log('request : ',requestBody);
+	var ref = firebase.database().ref('leave/'+requestBody.filters.users);
+
+	ref.once('value', function (snap) {
+	 	snap.forEach(function (parentSnap) {
+	 		responseData.push(parentSnap.val());
+	 	});
+	 	console.log('Response Data : ',responseData);
+	 	leaves = {
+	 		"leaves" : responseData
+	 	};
+
+	 	var dataResponse = {
+	 			"status" : "success",
+				"message" : "200 OK",
+				"data" : leaves
+		 	}
+
+		console.log('Return Data : ',dataResponse);
+
+	 	response.status(200).send(dataResponse); 
+
+	},function (error) {
+		console.log("Error: " + error.code);
+	});
+	}
+	else
+	{
+		return_value=[
+		{
+			"status" : "error",
+			"message" : "wrong method",
+		}]
+		response.status(200).send(return_value); 
+	}
+});
+
+
+exports.displayLeaveTest = functions.https.onRequest((request,response) => {
+	var return_value={
+				"status" : "success",
+				"message" : "200 OK",
+				"data" : {
+					"leaves": [
+					{
+						"user":{
+								"user_id": 80,
+								"name": "anish",
+								"email": "anish@ajency.in"
+							},
+						"leave_note": "sick",
+						"leave_date": [
+							"2018-04-12",
+							"2018-04-13"
+						],
+						"leave_parent_id": 12345,
+						"type": "leave_taken",
+						"tagged_users": [
+							{
+								"user_id": 80,
+								"name": "anish",
+								"email": "anish@ajency.in"
+							},
+							{
+								"user_id": 45,
+								"name": "nutan",
+								"email": "nutan@ajency.in"
+							}
+						],
+						"comments": [
+							{
+								"id": 1231,
+								"message": "hiii",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "nutan",
+										"email": "nutan@ajency.in"
+									},
+								"timestamp": "123123123"
+							}
+						]
+					},
+					{
+						"user": 
+							{
+								"user_id": 45,
+								"name": "nutan",
+								"email": "nutan@ajency.in"
+							},
+						"leave_note": "sick",
+						"leave_date": [
+							"2018-04-14",
+							"2018-04-15",
+							"2018-04-16"
+
+						],
+						"leave_parent_id": 12345,
+						"type": "leave_taken",
+						"tagged_users": [
+							{
+								"user_id": 80,
+								"name": "anish",
+								"email": "anish@ajency.in"
+							},
+							{
+								"user_id": 45,
+								"name": "nutan",
+								"email": "nutan@ajency.in"
+							},
+							{
+								"user_id": 81,
+								"name": "shreya",
+								"email": "shreya@ajency.in"
+							}
+						],
+						"comments": [
+							{
+								"id": 1231,
+								"message": "hiii",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "nutan",
+										"email": "nutan@ajency.in"
+									},
+								"timestamp": "123123123"
+							}
+						]
+					},
+					{
+						"user": 
+							{
+								"user_id": 81,
+								"name": "shreya",
+								"email": "shreya@ajency.in"
+							},
+						"leave_note": "I have a wedding to attend",
+						"leave_date": [
+							"2018-04-14",
+							"2018-04-15",
+							"2018-04-16"
+
+						],
+						"leave_parent_id": 12345,
+						"type": "leave_taken",
+						"tagged_users": [
+							{
+								"user_id": 80,
+								"name": "anish",
+								"email": "anish@ajency.in"
+							},
+							{
+								"user_id": 25,
+								"name": "tanaya",
+								"email": "bac@ajency.in"
+							},
+							{
+								"user_id": 15,
+								"name": "tanvi",
+								"email": "aa@ajency.in"
+							},
+							{
+								"user_id": 33,
+								"name": "prajay",
+								"email": "ss@ajency.in"
+							},
+							{
+								"user_id": 62,
+								"name": "sujit",
+								"email": "ds@ajency.in"
+							},
+							{
+								"user_id": 45,
+								"name": "nutan",
+								"email": "nutan@ajency.in"
+							},
+							{
+								"user_id": 81,
+								"name": "shreya",
+								"email": "shreya@ajency.in"
+							},
+							{
+								"user_id": 22,
+								"name": "sharang",
+								"email": "uu@ajency.in"
+							}
+						],
+						"comments": [
+							{
+								"id": 1231,
+								"message": "This is test",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "tanvi",
+										"email": "tanvi@ajency.in"
+									},
+								"timestamp": "123123123"
+							},
+							{
+								"id": 1231,
+								"message": "test2",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "amit",
+										"email": "amit@ajency.in"
+									},
+								"timestamp": "123123123"
+							},
+							{
+								"id": 1231,
+								"message": "test3",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "sujit",
+										"email": "nutan@ajency.in"
+									},
+								"timestamp": "123123123"
+							},
+							{
+								"id": 1231,
+								"message": "comment test4",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "sharang",
+										"email": "nutan@ajency.in"
+									},
+								"timestamp": "123123123"
+							}
+						]
+					},
+					{
+						"user": {
+								"user_id": 35,
+								"name": "sujay",
+								"email": "sujay@ajency.in"
+							},
+						"leave_note": "sick",
+						"leave_date": [
+							"2018-04-12"
+						],
+						"leave_parent_id": 5022233,
+						"type": "leave_taken",
+						"tagged_users": [
+							{
+								"user_id": 80,
+								"name": "anish",
+								"email": "anish@ajency.in"
+							}
+						],
+						"comments": [
+							{
+								"id": 1231,
+								"message": "hiii",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "nutan",
+										"email": "nutan@ajency.in"
+									},
+								"timestamp": "123123123"
+							},
+							{
+								"id": 1231,
+								"message": "hiii1",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "viraj",
+										"email": "nutan@ajency.in"
+									},
+								"timestamp": "123123123"
+							},
+							{
+								"id": 1231,
+								"message": "hiii2",
+								"user": 
+									{
+										"user_id": 45,
+										"name": "prajay",
+										"email": "nutan@ajency.in"
+									},
+								"timestamp": "123123123"
+							}
+						]
+					},
+					{
+						"user": {
+								"user_id": 35,
+								"name": "sujay",
+								"email": "sujay@ajency.in"
+							},
+						"leave_note": "sick",
+						"leave_date": [
+							"2018-04-12"
+						],
+						"leave_parent_id": 5022233,
+						"type": "leave_taken",
+						"tagged_users": [
+							{
+								"user_id": 80,
+								"name": "anish",
+								"email": "anish@ajency.in"
+							}
+						],
+						"comments": []
+					}
+					]
+				}
+	}
+	response.status(200).send(return_value); 
+});
+
+
+var db = firebase.firestore();
+
+exports.cloudLeave = functions.https.onRequest((request,response) => {
+
+	var cloudData = [];
+	// var id = ["81","80","45"];
+	var id = "81";
+	// var dateDa = new Date('2018-04-20'); 
+	const dataForDisplay = db.collection("leave_management").doc({id}).collection("812563");
+
+	dataForDisplay.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data());
+          //console.log(querySnapshot.doc().collection("comments"));
+        cloudData.push(doc.data());
+    });
+        response.status(200).send(cloudData); 
+        return 0;
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef);
+    return 1;
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+});
