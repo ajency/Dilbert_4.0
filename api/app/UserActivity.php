d0c726fe40febdb01d77c23f4b4af18efa5163b8
Sharang Sinai Amonkar <sharang@ajency.in>
2018-05-07 16:39:53 +0530

    add function to calculate the total time based on the user's activity

--- api/app/UserActivity.php
+++ api/app/UserActivity.php
@@ -6,5 +6,65 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserActivity extends Model
 {
-    //
+    /**
+     * calculates the total time based on the user activity table
+     * @param  [type]  $userId       user's id
+     * @param  [type]  $workDate     date for which to calculate the total time
+     * @param  boolean $workFromHome to consider only office activity or other as well
+     *                               default false
+     * @param  [type]  $userActivity array of prefetched user activity | optional
+     * @return string                in the "hh:mm" format ex: "12:00"
+     */
+    public function totalTimeForUser($userId, $workDate, $workFromHome = false, $userActivity = null) {
+        $totalMins = 0;
+        /**
+         * check if the user activity is already passed
+         */
+        if($userActivity == null) {
+            // fetch the user's activity
+            $userActivity = $this::where([
+                'user_id' => $userId,
+                'work_date' => $workDate,
+            ])->get();
+        }
+
+        /**
+         * Calculate the total office
+         */
+        $firstOfficeEntry = $userActivity->where('type','office')->first();
+        $lastOfficeEntry = $userActivity->where('type','office')->last();
+
+        if($firstOfficeEntry != null)   // i.e. both the first and the last entry exists
+            $totalMins = $totalMins + (new Carbon($firstOfficeEntry->from))->diffInMinutes(new Carbon($lastOfficeEntry->to));
+
+        /**
+         * Include the 'other' time if $workFromHome is true
+         */
+        if($workFromHome) {
+            $otherEntries = $userActivity->where('type','other');
+
+            // if first and last office entries are null, consider all home entries
+            // as it means the user has worked from home the whole time
+            if($firstOfficeEntry == null)
+                $takeAllEntries = true;
+            else
+                $takeAllEntries = false;
+
+            foreach($otherEntries as $otherEntry) {
+                if($takeAllEntries || (
+                    new Carbon($otherEntry->to) <= new Carbon($firstOfficeEntry->from) ||
+                    new Carbon($otherEntry->from) >= new Carbon($lastOfficeEntry->to)
+                    )   // i.e. the 'other' activity lies outside of the entire 'office' period
+                ) {
+                    // then calculate the time difference and add to the total time
+                    $totalMins = $totalMins + (new Carbon($otherEntry->from))->diffInMinutes(new Carbon($otherEntry->to));
+                }
+            }
+        }
+
+        /**
+         * Convert and return the total time to a "hh:mm" string
+         */
+         return str_pad((int)($totalMins / 60),2,'0',STR_PAD_LEFT).":".str_pad(($totalMins % 60),2,'0',STR_PAD_LEFT);
+    }
 }
