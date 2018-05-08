fa0f28bfb51991050f6038053361a9c7695fa3e8
sujitgawas19 <sujit@ajency.in>
2018-05-08 12:51:19 +0530

    dilbert_leave_view build

--- app/platforms/browser/www/cordova_plugins.js
+++ app/platforms/browser/www/cordova_plugins.js
@@ -58,7 +58,7 @@ module.exports.metadata =
     "cordova-plugin-console": "1.1.0",
     "cordova-plugin-device": "1.1.7",
     "cordova-plugin-splashscreen": "4.1.0",
-    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-statusbar": "2.4.1",
     "cordova-plugin-whitelist": "1.3.3",
     "ionic-plugin-keyboard": "2.2.1"
 }
